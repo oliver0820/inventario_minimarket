e36ae584af61677891c0249ed825f3427d6e5321
oliver0820 <159640190+oliver0820@users.noreply.github.com>
2025-04-04 10:05:55 -0400

    agregar funcionalidad para eliminar productos del inventario

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 
+function eliminarProducto(index) {
+  let productos = JSON.parse(localStorage.getItem('productos')) || [];
+  productos.splice(index, 1);
+  localStorage.setItem('productos', JSON.stringify(productos));
+  mostrarProductos();
+}
